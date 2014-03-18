26aed483c1217cae4e9b2c2c5a0f733c6f346308
Jana Petrova <janavpetrova@gmail.com>
2014-03-18 16:11:13 +0000

    refactored searcher

--- lib/sweeper/searcher.rb
+++ lib/sweeper/searcher.rb
@@ -2,8 +2,6 @@ class Sweeper::Searcher
   attr_reader :filenames
 
   def initialize(paths)
-    @filenames = []
-
     files_and_dirs = paths.map{|path| Dir.glob("#{path}/**/*") }.flatten
 
     @filenames = files_and_dirs.reject{ |file| File.directory? file }
