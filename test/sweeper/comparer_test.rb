b32b8e63cd5b61dca2376fa55be3f9251436ecd4
Jana Petrova <janavpetrova@gmail.com>
2014-04-15 18:44:17 +0000

    refactored comparer

--- test/sweeper/comparer_test.rb
+++ test/sweeper/comparer_test.rb
@@ -2,14 +2,14 @@ require "test_helper"
 
 class Sweeper::ComparerTest < MiniTest::Unit::TestCase
   def setup
-    @parsed_filenames = ["ui/orange.js.coffee", "app.js.coffee"]
+    @parsed_filenames = ["ui/orange", "app"]
     @searched_filenames = @parsed_filenames + ["extra-file"]
 
-    @comparer = Sweeper::Comparer.new(@parsed_filenames, @searched_filenames)
+    @compared = Sweeper::Comparer.compare(@parsed_filenames, @searched_filenames)
   end
 
   def test_should_compare_filenames
-    assert { @comparer.files.class == Array }
-    assert { @comparer.files == (@searched_filenames - @parsed_filenames) }
+    assert { @compared.class == Array }
+    assert { @compared == (@searched_filenames - @parsed_filenames) }
   end
 end
