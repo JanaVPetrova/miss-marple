1761b417d323a76f9f949479971047ea6c784db4
Jana Petrova <janavpetrova@gmail.com>
2014-04-18 16:34:13 +0000

    added putting undeclared in manifest filenames

--- lib/sweeper/comparer.rb
+++ lib/sweeper/comparer.rb
@@ -1,6 +1,15 @@
 class Sweeper::Comparer
   def self.compare(parsed, searched)
     searched.map!{|fname| fname.split(".").first }
-    searched - parsed
+
+    searched.reject{ |fname| search_similar(parsed, fname) }
+  end
+
+  private
+  def self.search_similar(arr, searched_fname)
+    arr.each do |fname|
+      return true if searched_fname.match(/#{fname}$/)
+    end
+    return false
   end
 end
