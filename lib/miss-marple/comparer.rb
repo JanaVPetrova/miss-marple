dd800c414fbc1a64095e9977d816fb41c2e89c97
Jana Petrova <janavpetrova@gmail.com>
2014-05-02 07:35:04 +0000

    fix extentions

--- lib/miss-marple/comparer.rb
+++ lib/miss-marple/comparer.rb
@@ -1,14 +1,13 @@
 class MissMarple::Comparer
   def self.compare(parsed, searched)
-    searched.map!{|fname| fname.split(".").first }
-
     searched.reject{ |fname| search_similar(parsed, fname) }
   end
 
   private
   def self.search_similar(arr, searched_fname)
     arr.each do |path|
-      if searched_fname.end_with?(path) || searched_fname.start_with?(path)
+      path_without_ext = path.split(".").first
+      if searched_fname.end_with?(path_without_ext) || searched_fname.start_with?(path_without_ext)
         return true
       end
     end
