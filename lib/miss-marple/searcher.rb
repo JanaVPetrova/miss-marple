9acbcbe3c1c99b52bce7d0e4f33474f40e72c982
Jana Petrova <janavpetrova@gmail.com>
2014-05-02 09:15:11 +0000

    refactoring: replased ugly method with correct matcher

--- lib/miss-marple/searcher.rb
+++ lib/miss-marple/searcher.rb
@@ -1,10 +1,5 @@
 class MissMarple::Searcher
   def self.search(paths)
-    files_and_dirs = paths.map{|path| Dir.glob("#{path}/**/*") }.flatten
-
-    permited_exts = %(.css .js .js.coffee .less .scss .sass)
-
-    no_dirs = files_and_dirs.reject{ |file| File.directory?(file) }
-    no_dirs.reject{|file| !permited_exts.include? File.extname(file) }
+    Dir.glob("**/*.{*css,js,coffee,[sa,sc,le]ss}")
   end
 end
